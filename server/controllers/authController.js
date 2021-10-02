f8576cc612d697f1459357ab6d8fe2b73eb0bdf7
axie0320 <axie0320@gmail.com>
2021-10-01 22:43:37 -0400

    Updated user model to include new property newMessage. The property is set to true only if this user has received a new request message from someone.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,23 +1,23 @@
-const models = require("../models/pfaModels")
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
+const models = require('../models/pfaModels');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
-const authController = {}
+const authController = {};
 
 function validateEmail(str) {
   const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(String(str).toLowerCase())
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(str).toLowerCase());
 }
 
 authController.verifyUser = async (req, res, next) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
     if (!email || !password) {
       res.locals.verification = {
         hasLogged: false,
-      }
-      return next()
+      };
+      return next();
     }
 
     // object specifying the fields to be requested from db
@@ -27,57 +27,66 @@ authController.verifyUser = async (req, res, next) => {
       lastName: 1,
       email: 1,
       isAdmin: 1,
-    }
+      newMessage: 1,
+    };
+
     const verification = {
       hasLogged: false,
-    }
-    const user = await models.User.findOne({ email })
+    };
+    
+    const user = await models.User.findOne({ email });
 
     if (!user || (await user.verify(password)) === false) {
-      verification.hasLogged = false
-      res.locals.verification = verification
-      return next()
+      verification.hasLogged = false;
+      res.locals.verification = verification;
+      return next();
     } else if (user && (await user.verify(password)) === true) {
-      verification.hasLogged = true
-      verification.userInfo = {}
+      verification.hasLogged = true;
+      verification.userInfo = {};
       for (const key in specifiedFields) {
-        verification.userInfo[key] = user[key]
+        verification.userInfo[key] = user[key];
       }
-      res.locals.verification = verification
-      return next()
+
+      if (user.newMessage) {
+        await models.User.updateOne({ email }, {$set: {newMessage: false}});
+      }
+
+      res.locals.verification = verification;
+      return next();
     }
   } catch (err) {
     return next({
-      log: "Express error handler caught an error at authController.verifyUser",
+      log: 'Express error handler caught an error at authController.verifyUser',
       message: { err },
-    })
+    });
   }
-}
+};
+
 authController.createUser = async (req, res, next) => {
   try {
-    const { email, password, firstName, lastName, skillsToTeach } = req.body
+    const { email, password, firstName, lastName, skillsToTeach } = req.body;
     const verification = {
       hasLogged: false,
-    }
+    };
     // console.log(req.body);
     if (!email || !password || !firstName || !lastName) {
       res.locals.verification = {
-        hasLogged: "empty",
-      }
-      return next()
+        hasLogged: 'empty',
+      };
+      return next();
     }
     if (!validateEmail(email)) {
-      verification.hasLogged = "format"
-      res.locals.verification = verification
-      return next()
+      verification.hasLogged = 'format';
+      res.locals.verification = verification;
+      return next();
     }
 
-    const teach = []
+    const teach = [];
     for (const key in skillsToTeach) {
       teach.push({
         name: key,
         _id: mongoose.Types.ObjectId(skillsToTeach[key]),
-      })
+      });
     }
 
     // object specifying the filters on query
@@ -87,7 +96,7 @@ authController.createUser = async (req, res, next) => {
       firstName,
       lastName,
       teach,
-    }
+    };
 
     // console.log('user doc submit:', userDoc);
     // object specifying the fields to be returned from db
@@ -96,16 +105,17 @@ authController.createUser = async (req, res, next) => {
       lastName: 1,
       email: 1,
       isAdmin: 1,
-    }
+      newMessage: 1,
+    };
     // console.log('hit db lines');
-    const emailExist = await models.User.findOne({ email })
+    const emailExist = await models.User.findOne({ email });
 
     if (emailExist) {
-      res.locals.verification = verification
-      return next()
+      res.locals.verification = verification;
+      return next();
     }
 
-    const user = await models.User.create(userDoc)
+    const user = await models.User.create(userDoc);
 
     // update teachers in skill to reflect the new user
     const newTeacher = {
@@ -113,56 +123,56 @@ authController.createUser = async (req, res, next) => {
       lastName,
       email,
       _id: user._id,
-    }
+    };
 
-    const skills = Object.keys(skillsToTeach)
+    const skills = Object.keys(skillsToTeach);
     if (skills.length != 0) {
       await models.Skill.updateMany(
         { name: { $in: skills } },
         { $push: { teachers: newTeacher } }
-      )
+      );
     }
 
-    verification.hasLogged = true
-    verification.userInfo = {}
+    verification.hasLogged = true;
+    verification.userInfo = {};
 
     // pull requested fields from user info returned from db
     for (const key in specifiedFields) {
-      verification.userInfo[key] = user[key]
+      verification.userInfo[key] = user[key];
     }
 
-    res.locals.verification = verification
+    res.locals.verification = verification;
     // console.log('verification: ', verification);
-    return next()
+    return next();
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return next({
-      log: "Express error handler caught an error at authController.verifyUser",
+      log: 'Express error handler caught an error at authController.verifyUser',
       message: { err },
-    })
+    });
   }
-}
+};
 
 authController.createSession = async (req, res, next) => {
   try {
     if (res.locals.verification.hasLogged !== true) {
-      return next()
+      return next();
     }
-    const token = await jwt.sign({ id: req.body.email }, process.env.ID_SALT)
-    res.cookie("ssid", token, { maxAge: 300000 })
-    return next()
+    const token = await jwt.sign({ id: req.body.email }, process.env.ID_SALT);
+    res.cookie('ssid', token, { maxAge: 300000 });
+    return next();
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
 authController.verifyToken = async (req, res, next) => {
   try {
-    const token = req.body.token
-    const isToken = await jwt.verify(token, process.env.ID_SALT)
+    const token = req.body.token;
+    const isToken = await jwt.verify(token, process.env.ID_SALT);
     if (isToken.id) {
-      console.log("isToken is", isToken)
-      res.locals.tokenVerif = true
+      console.log('isToken is', isToken);
+      res.locals.tokenVerif = true;
 
       // const queryFilter = {
       //   targetEmail: isToken.id,
@@ -180,14 +190,14 @@ authController.verifyToken = async (req, res, next) => {
       // await models.Message.updateMany(queryFilter, updateFields);
 
       // res.locals.messages = messages;
-    } else res.locals.tokenVerif = false
-    return next()
+    } else res.locals.tokenVerif = false;
+    return next();
   } catch (err) {
     return next({
-      log: "Express error handler caught an error at authController.verifyToken",
+      log: 'Express error handler caught an error at authController.verifyToken',
       message: { err },
-    })
+    });
   }
-}
+};
 
-module.exports = authController
+module.exports = authController;
