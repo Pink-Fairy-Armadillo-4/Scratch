81de813f390200627fc2608221e59560324bf237
Yochi Shor <moebochur@gmail.com>
2021-10-06 11:36:53 -0400

    Global error handler and some security

--- server/app.js
+++ server/app.js
@@ -1,15 +1,43 @@
 const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const authRouter = require('./routes/auth');
-const apiRouter = require('./routes/api');
+const morgan = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
+
+const apiRouter = require('./routes/api');
+const authRouter = require('./routes/auth');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
+// GLOBAL MIDDLEWARES
+// Serving static files
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// Development logging
+if (process.env.NODE_ENV === 'developement') {
+  app.use(morgan('dev'));
+}
+
+// Body parser; reading data from the body into req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// ROUTES
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
+
+// Handle 404
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global error handler
+app.use(globalErrorHandler);
 
 module.exports = app;
