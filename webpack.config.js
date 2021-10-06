9b856ad7079bb8e22eb57d3765f7c232ef4e4a06
Robert Crocker <robert@vizsimply.com>
2021-10-05 23:17:13 -0300

    Updated the webpack and env.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,28 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const webpack = require("webpack")
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin")
 
 module.exports = {
-  entry: [
-    './client/index.js'
-  ],
+  entry: ["./client/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),npm
-    filename: 'bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
-  mode: 'development',
+  mode: "development",
   // devtool: 'eval-source-map',
   devServer: {
     port: 8080,
     open: true,
     historyApiFallback: true,
     proxy: {
-      '/api/**': {
-        target: 'http://localhost:3000/',
+      "/api/**": {
+        target: "http://localhost:3000/",
       },
-      '/auth/**': {
-        target: 'http://localhost:3000/',
-      }
-    }
+      "/auth/**": {
+        target: "http://localhost:3000/",
+      },
+    },
   },
   module: {
     rules: [
@@ -33,42 +30,42 @@ module.exports = {
         test: /.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
         },
       },
       {
         test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /.(css|scss)$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './client/index.html',
-    },),
+      template: "./client/index.html",
+    }),
     new ImageMinimizerPlugin({
       minimizerOptions: {
         // Lossless optimization with custom option
         // Feel free to experiment with options for better result for you
         plugins: [
-          ['gifsicle', { interlaced: true }],
-          ['jpegtran', { progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
         ],
       },
     }),
   ],
   resolve: {
     // Enable importing JS / JSX files without specifying their extension
-    extensions: ['.js', '.jsx']
-  }
-};
+    extensions: [".js", ".jsx"],
+  },
+}
