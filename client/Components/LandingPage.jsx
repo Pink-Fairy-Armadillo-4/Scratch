970c11b7ead19ff940c2d187fed12ad03a10d04f
Robert Crocker <robert@vizsimply.com>
2021-10-06 21:15:55 -0300

    Added global styles.

--- client/Components/LandingPage.jsx
+++ client/Components/LandingPage.jsx
@@ -1,22 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import PopUpLog from './PopUpLog';
-import PopUpReg from './PopUpReg';
+import React, { useState, useEffect } from "react"
+import PopUpLog from "./PopUpLog"
+import PopUpReg from "./PopUpReg"
+
+// Styles
+import styled from "styled-components"
 
 const LandingPage = (props) => {
   //states to open/close modal, passed as props to PopUpLog, PopUpReg components;
-  const [seenLog, setSeenLog] = useState(false);
-  const [seenSignUp, setSeenSignUp] = useState(false);
+  const [seenLog, setSeenLog] = useState(false)
+  const [seenSignUp, setSeenSignUp] = useState(false)
 
-  // functions on click to modify state of PopUpLog, PopUpReg 
+  // functions on click to modify state of PopUpLog, PopUpReg
   // and open/close modals. condit rendering
   const togglePopLog = () => {
-    seenLog ? setSeenLog(false) : setSeenLog(true);
-    seenSignUp ? setSeenSignUp(false) : null;
-  };
+    seenLog ? setSeenLog(false) : setSeenLog(true)
+    seenSignUp ? setSeenSignUp(false) : null
+  }
   const togglePopReg = () => {
-    seenSignUp ? setSeenSignUp(false) : setSeenSignUp(true);
-    seenLog ? setSeenLog(false) : null;
-  };
+    seenSignUp ? setSeenSignUp(false) : setSeenSignUp(true)
+    seenLog ? setSeenLog(false) : null
+  }
+
+  const Button = styled.button`
+    color: red;
+  `
 
   return (
     <div className="parent landing-page-bg">
@@ -39,16 +46,18 @@ const LandingPage = (props) => {
           Connect with your cohort. They have a lot to teach.
         </main>
         <div className="btn-group">
-          <button className="btn" onClick={togglePopLog}>
+          {/* Example of an alternative approach */}
+          {/* onClick{() => setSeenLog(!seenLog)} */}
+          <Button className="btn" onClick={togglePopLog}>
             Login
-          </button>
+          </Button>
           <button className="btn" onClick={togglePopReg}>
             Signup
           </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LandingPage;
+export default LandingPage
