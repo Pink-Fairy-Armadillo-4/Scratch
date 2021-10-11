83de3b412d3ac6bf43fd8eba7c7eb713231761cd
Yochi Shor <moebochur@gmail.com>
2021-10-11 13:52:40 -0400

    Created message model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,34 +1,62 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const capitalize = require('../utils/capitalize');
 const { Schema } = mongoose;
 
 // sets a schema for the 'user' collection
-const userSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  isAdmin: { type: Boolean, required: true, default: false },
-  userGroup: { type: String, default: 'user' },
-  newMessage: { type: Boolean, required: true, default: false },
-  teach: [
-    {
-      name: String,
-      _id: {
-        type: Schema.Types.ObjectId,
-        ref: 'skill',
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    // Name field created automatically based off firstName and Lastname
+    name: String,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+    userGroup: { type: String, default: 'user' },
+    newMessage: { type: Boolean, required: true, default: false },
+    teach: [
+      {
+        name: String,
+        _id: {
+          type: Schema.Types.ObjectId,
+          ref: 'skill',
+        },
       },
-    },
-  ],
-  learn: [
-    {
-      name: String,
-      _id: {
-        type: Schema.Types.ObjectId,
-        ref: 'skill',
+    ],
+    learn: [
+      {
+        name: String,
+        _id: {
+          type: Schema.Types.ObjectId,
+          ref: 'skill',
+        },
       },
-    },
-  ],
+    ],
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+userSchema.pre('save', function (next) {
+  this.firstName = capitalize(this.firstName);
+  this.lastName = capitalize(this.lastName);
+  this.name = `${this.firstName} ${this.lastName}`;
+
+  next();
+});
+
+// Virtual populate
+userSchema.virtual('messagesSent', {
+  ref: 'Message',
+  foreignField: 'from',
+  localField: '_id',
+});
+
+userSchema.virtual('messagesReceived', {
+  ref: 'Message',
+  foreignField: 'to',
+  localField: '_id',
 });
 
 //set up preprocess for encrypting password
